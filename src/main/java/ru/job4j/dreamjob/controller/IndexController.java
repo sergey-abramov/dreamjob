ab26bc86e882bbb70f9c05afa1f8330e6d661711
LESHA <belochuvach@gmail.com>
2023-04-01 11:30:38 +0300

    4. Thymeleaf, Циклы. [#504841]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -3,7 +3,6 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class IndexController {
 
