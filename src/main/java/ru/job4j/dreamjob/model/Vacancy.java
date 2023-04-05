4c27182ad719a7f1a8c2d4ed4d2ba990518843eb
LESHA <belochuvach@gmail.com>
2023-04-05 11:06:32 +0300

    Создание вакансии

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -9,7 +9,10 @@ public class Vacancy {
 
     private String title;
     private String description;
-    private LocalDateTime creationDate;
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    public Vacancy() {
+    }
 
     public Vacancy(int id, String title) {
         this.id = id;
